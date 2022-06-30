3a92a3b7dbd49d964467ee0e5572cdc0e71fba89
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-06-30 14:48:57 -0700

    update doc ready function to chain together promises

---
+++ src/giphy-service.js
@@ -0,0 +1,14 @@
+export default class GiphyService {
+  static getGif(word) {
+    return fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${word}&limit=20`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,30 +3,64 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import DictionaryService from "./dictionary-service.js";
-
+import GiphyService from "./giphy-service.js";
 
 function clearFields() {
   $("#word").val("");
+  $('.showErrors').text("");
+  
 }
 
-function getElements(response) {
-  if (response) {
-    $(".showPartOfSpeech").text(`Part of Speech:${response[0].meanings[0].partOfSpeech}` );
-    $(".showDefinition").text(`Definition:${response[0].meanings[0].definitions[0].definition}` );
-    $(".showSynonym").text(`Synonyms:${response[0].meanings[0].synonyms}` );
+function displayDefinition(partOfSpeech,definition, synonyms) {
+  $(".showPartOfSpeech").text(`Part of Speech: ${partOfSpeech}` );
+  $(".showDefinition").text(`Definition: ${definition}` );
+  $(".showSynonym").text(`Synonyms: ${synonyms}` );
+  // let definitionArray = description[0].meanings[0].definitions;
+  // let myJSON;
+  // definitionArray.forEach(function (element) {
+  //   console.log(element);
     
-  } else {
-    $(".showErrors").text(`There was an error: ${response.message}`);
-  }
+  //   let myJSON = JSON.stringify(element);
+  //   return myJSON;
+    
+  // });
+  // console.log(myJSON);
+  
+  // $(".showDefLoop").html(myJSON); ///<p>element.definition</p>
 }
+
+function displayGif(response) {
+  $(".showGif").html(`<img src="${response.data[1].images.downsized_medium.url}"/>`);
+}
+
+function displayErrors(error) {
+  $('.showErrors').text(`${error}`);
+}
+
 
 $(document).ready(function () {
   $("#getDefinition").click(function () {
     let word = $("#word").val();
     clearFields();
     DictionaryService.getDefinition(word)
-      .then(function (response) {
-        getElements(response);
+      .then(function (dictionaryResponse) {
+        if (dictionaryResponse instanceof Error) {
+          throw Error (`Free Dictionary API error: ${dictionaryResponse.message}`);
+        }
+        const definitionDescription = dictionaryResponse[0].meanings[0].definitions[0].definition;
+        const partOfSpeechDescrition = dictionaryResponse[0].meanings[0].partOfSpeech;
+        const synonymsDescription = dictionaryResponse[0].meanings[0].synonyms;
+        displayDefinition(partOfSpeechDescrition, definitionDescription, synonymsDescription);
+        return GiphyService.getGif(word);
+      })
+      .then(function (giphyResponse) {
+        if (giphyResponse instanceof Error) {
+          throw Error (`Giphy API error: ${giphyResponse.message}`);
+        }
+        displayGif(giphyResponse);
+      })
+      .catch(function(error){
+        displayErrors(error.message);
       });
   });
 });
